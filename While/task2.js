aab41f9e7ecabb51a4056a871623ef83bd9bb618
Tanveer Hasan Rishad <tanveerrishad007@gmail.com>
2026-01-30 17:11:16 +0600

    completed 2nd task

---
+++ While/task2.js
@@ -0,0 +1,18 @@
+let n = 61;
+while (n <= 100) {
+  if (n % 2 != 0) {
+    console.log(n);
+  }
+  n++;
+}
+
+console.log();
+
+let x = 78;
+
+while (x <= 98) {
+  if (x % 2 == 0) {
+    console.log(x);
+  }
+  x++;
+}
