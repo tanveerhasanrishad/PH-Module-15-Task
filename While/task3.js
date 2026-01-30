654d17f5416006450816e7d2f4159da287d56d1e
Tanveer Hasan Rishad <tanveerrishad007@gmail.com>
2026-01-30 17:14:30 +0600

    completed 3rd task

---
+++ While/task3.js
@@ -0,0 +1,20 @@
+let sum1 = 0;
+let n = 81;
+
+while (n <= 131) {
+  sum1 = sum1 + n;
+  n++;
+}
+console.log("sum of all the odd numbers from 81 to 131: " + sum1);
+
+console.log();
+
+let sum2 = 0;
+let x = 206;
+
+while (x <= 311) {
+  sum2 = sum2 + x;
+  x++;
+}
+
+console.log("sum of all the even numbers from 206 to 311: " + sum2);
