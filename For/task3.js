f0777443f2a5487ce60929bcc9df8110a1ebb429
Tanveer Hasan Rishad <tanveerrishad007@gmail.com>
2026-01-30 17:28:54 +0600

    completed for loop 3rd task

---
+++ For/task3.js
@@ -0,0 +1,16 @@
+let sum1 = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 != 0) {
+    sum1 = sum1 + i;
+  }
+}
+
+console.log("sum of all the odd numbers from 91 to 129: " + sum1);
+
+let sum2 = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 == 0) {
+    sum2 = sum2 + i;
+  }
+}
+console.log("sum of all the even numbers from 51 to 85: " + sum2);
